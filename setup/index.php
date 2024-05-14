c37b8af8033802604d5ecb9df3e1d39e69ecc326
kanimomonga <yu.isa45.2@gmail.com>
2024-05-15 00:23:06 +0900

    Update index.php

--- setup/index.php
+++ setup/index.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
 
-// データベースファイルのパス
-$dbFile = __DIR__ . '/../pharmacy.db';
-// メール設定ファイルのパス
-$mailConfigFile = __DIR__ . '/../mail_config.json';
 // 関数群の読み込み
 require_once __DIR__ . '/../functions.php';
 
-if (isSetupComplete($dbFile, $mailConfigFile)) {
+if (isSetupComplete()) {
     // セットアップが完了している場合
     $_SESSION['setup_message'] = "セットアップは既に完了しています。";
     header("Location: setup_message.php");
